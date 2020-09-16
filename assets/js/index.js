fffa1fefc71ee9b2ee99d9b5d2dca899d582ce82
ml7757 <matthew.laporte@tutanota.com>
2020-09-16 11:04:58 +0200

    add more button functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
 	$("#js-scroll-to-comments").click(function() {
 		$('html, body').animate({scrollTop: $('.js-section-comments').offset().top}, 1000);
 	});
+
+	if($(".card__hidden:hidden").length < 6) {$("#loadMore").hide();}
+
+	$(".card__hidden").slice(0, 6).show();
+
+	$("#loadMore").on('click', function (e) {
+		e.preventDefault();
+
+		$(".card__hidden:hidden").slice(0, 6).slideDown();
+		if($(".card__hidden:hidden").length == 0) {
+			$("#loadMore").fadeOut();
+		}
+
+		$('html, body').animate({scrollTop: $().offset().top}, 1000);
+	});
 });
