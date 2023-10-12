b57b6a504bc58ee472e5eecc195875bf148f7339
ml7757 <matthew.laporte@tutanota.com>
2023-10-12 06:52:35 +0200

    rm mehr artikel btn and jquery func

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,8 @@
 $(document).ready(function() {
 
-  $('#bread a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('breadcrumb__active');
+	$('#bread a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('breadcrumb__active');
 
 	$("#js-scroll-to-comments").click(function() {
 		$('html, body').animate({scrollTop: $('.js-section-comments').offset().top}, 1000);
-	});
-
-	if($(".card__hidden:hidden").length <= 12) {$("#loadMore").hide();}
-
-	$(".card__hidden").slice(0, 11).show();
-
-	$("#loadMore").on('click', function (e) {
-		e.preventDefault();
-
-		$(".card__hidden:hidden").slice(0, 6).slideDown();
-		if($(".card__hidden:hidden").length == 0) {
-			$("#loadMore").fadeOut();
-		}
-
-		$('html, body').animate({scrollTop: $(this).offset().bottom}, 1000);
 	});
 });
